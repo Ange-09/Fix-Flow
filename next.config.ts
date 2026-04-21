5620b5bb5a69e399d57954192cc3b79f686e7210
Ange-09 <santiago.ry.angelo@gmail.com>
2026-04-21 21:15:53 +0800

    asdf

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
   basePath: "/Fix-Flow",
   assetPrefix: "/Fix-Flow/",
 };
